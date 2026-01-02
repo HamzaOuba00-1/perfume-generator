2d9827cfa88347837e7a256b104d3ac86f410de0
HamzaOuba00-1 <hamza.ouba.2004@gmail.com>
2026-01-02 22:25:04 +0200

    Initial commit - Spring Boot project setup

---
+++ perfume-generator-backend/src/main/java/com/perfume/backend/PerfumeGeneratorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.perfume.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeGeneratorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeGeneratorBackendApplication.class, args);
+	}
+
+}
