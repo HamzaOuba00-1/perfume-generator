e41aa9abf737602aeb65b1b9c865d52ebadd7ed3
HamzaOuba00-1 <hamza.ouba.2004@gmail.com>
2026-02-03 21:40:45 +0200

    refactor(frontend): update app, config and routes

--- perfume-generator-frontend/src/app/app.routes.ts
+++ perfume-generator-frontend/src/app/app.routes.ts
@@ -1,21 +1,31 @@
 import { Routes } from '@angular/router';
-import { GeneratePerfumeComponent } from './pages/generate-perfume/generate-perfume.component';
-import { ResultPerfumeComponent } from './pages/result-perfume/result-perfume.component';
+import { GeneratePerfumeComponent } from './features/perfume/pages/generate-perfume/generate-perfume.component';
+import { ResultPerfumeComponent } from './features/perfume/pages/result-perfume/result-perfume.component';
 
 export const routes: Routes = [
 
   // ================= ADMIN =================
   {
     path: 'admin',
-    loadComponent: () =>
-      import('./pages/admin/admin-login/admin-login.component')
-        .then(m => m.AdminLoginComponent)
-  },
-  {
-    path: 'admin/dashboard',
-    loadComponent: () =>
-      import('./pages/admin/admin-dashboard/admin-dashboard.component')
-        .then(m => m.AdminDashboardComponent)
+    children: [
+
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./features/admin/pages/admin-login/admin-login.component')
+            .then(m => m.AdminLoginComponent)
+      },
+
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/admin/pages/admin-dashboard/admin-dashboard.component')
+            .then(m => m.AdminDashboardComponent)
+      },
+
+      // Default admin route
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
   },
 
   // ================= USER =================
